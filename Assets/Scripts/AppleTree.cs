197b2714dba02be696912c943aab0b6898867b0f
kevinjyx <kevinjyx@gmail.com>
2020-09-24 23:38:36 -0400

    update

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+  
+    public float SecondsBetweenAppleDrops = 1f;
+    void Start()
+    {
+        //drop apple
+        InvokeRepeating("DropApple", 2f, SecondsBetweenAppleDrops);
+    }
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //movement, change direction
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Math.Abs(speed);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/HighScores.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class HighScores : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static public int score = 1000;
+    void Start()
+    {
+        
+    }
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMesh scoreGT = this.GetComponent<TextMesh>();
+        scoreGT.text = "High Score: " + score;
+    }
+}
